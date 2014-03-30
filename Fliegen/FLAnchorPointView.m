700602eb0ac7d80cd4c7529ca2688d04557b6311
Abhishek Moothedath <lord.visionz@gmail.com>
2014-03-30 12:21:57 -0400

    selection handles on 3 axes

--- Fliegen/FLAnchorPointView.m
+++ Fliegen/FLAnchorPointView.m
@@ -27,48 +27,69 @@
 
 -(SCNNode*)setSelectionHandlesForRootNode:(SCNNode *)rootNode
 {
-    SCNCylinder *xAxisRod = [SCNCylinder cylinderWithRadius:.2 height:4];
+    SCNNode *selectionNode = [SCNNode node];
+    [selectionNode setName:@"selectionHandles"];
+    SCNCylinder *xAxisRod = [SCNCylinder cylinderWithRadius:.1 height:4];
     SCNNode *xAxisRodNode = [SCNNode nodeWithGeometry:xAxisRod];
-    SCNMaterial *material = [SCNMaterial material];
-    material.diffuse.contents = [NSColor greenColor];
-    [xAxisRod setFirstMaterial:material];
+    SCNMaterial *xAxisMaterial = [SCNMaterial material];
+    xAxisMaterial.diffuse.contents = [NSColor greenColor];
+    [xAxisRod setFirstMaterial:xAxisMaterial];
     
-//    CATransform3D xAxisTransform = self.worldTransform;
-    
-    
-//    CATransform3D xAxisTransform = CATransform3DMakeRotation(M_PI_2, 1, 0, 0);
-//    CATransform3D xAxisTransform = CATransform3DRotate(self.worldTransform, M_PI_2, 1, 0, 0);
-    
-//    xAxisTransform = CATransform3DConcat(self.worldTransform, xAxisTransform);
-    
-//    xAxisTransform = CATransform3DTranslate(xAxisTransform, 4, 0, 0);
-    
-//    CATransform3D xAxisTransform = CATransform3DTranslate(xAxisTransform, 0, 0, 4);
-//    xAxisTransform = CATransform3DRotate(xAxisTransform, M_PI_2, 1, 0, 0);
-    
-//    CATransform3D xAxisTransform = viewMatrix;
-    
-//    [xAxisRodNode setPosition:SCNVector3Make(4, 0, 0)];
-//    [xAxisRodNode setRotation:SCNVector4Make(1, 0, 0, M_PI_2)];
     
     SCNVector3 xAxisOffset = SCNVector3Make(self.worldTransform.m41, self.worldTransform.m42, self.worldTransform.m43);
 //    CATransform3D xAxisTransform = CATransform3DMakeRotation(M_PI_2, 1, 0, 0);
 //    xAxisTransform = CATransform3DTranslate(xAxisTransform, xAxisOffset.x, xAxisOffset.y, xAxisOffset.z);
     CATransform3D xAxisTransform = CATransform3DMakeTranslation(xAxisOffset.x, xAxisOffset.y, xAxisOffset.z);
     xAxisTransform = CATransform3DRotate(xAxisTransform, M_PI_2, 1, 0, 0);
-    xAxisTransform = CATransform3DTranslate(xAxisTransform, 0, 2, 1);
+    xAxisTransform = CATransform3DTranslate(xAxisTransform, 0, 2, 0);
     [xAxisRodNode setTransform:xAxisTransform];
 //    [self addChildNode:xAxisRodNode];
     
     SCNCone *xAxisCone = [SCNCone coneWithTopRadius:0 bottomRadius:.5 height:2];
     SCNNode *xAxisConeNode = [SCNNode nodeWithGeometry:xAxisCone];
-    [xAxisCone setFirstMaterial:material];
+    [xAxisCone setFirstMaterial:xAxisMaterial];
     [xAxisConeNode setPosition:SCNVector3Make(0, 2, 0)];
     [xAxisRodNode addChildNode:xAxisConeNode];
     
+    [selectionNode addChildNode:xAxisRodNode];
     
+    SCNCylinder *yAxisRod = [SCNCylinder cylinderWithRadius:.1 height:4];
+    SCNNode *yAxisRodNode = [SCNNode nodeWithGeometry:yAxisRod];
+    SCNMaterial *yAxisMaterial = [SCNMaterial material];
+    yAxisMaterial.diffuse.contents = [NSColor blueColor];
+    [yAxisRod setFirstMaterial:yAxisMaterial];
     
-    return xAxisRodNode;
+    CATransform3D yAxisTransform = CATransform3DMakeTranslation(xAxisOffset.x, xAxisOffset.y, xAxisOffset.z);
+    yAxisTransform = CATransform3DRotate(yAxisTransform, M_PI_2, 0, 1, 0);
+    yAxisTransform = CATransform3DTranslate(yAxisTransform, 0, 2, 0);
+    [yAxisRodNode setTransform:yAxisTransform];
+    
+    SCNCone *yAxisCone = [SCNCone coneWithTopRadius:0 bottomRadius:.5 height:2];
+    SCNNode *yAxisConeNode = [SCNNode nodeWithGeometry:yAxisCone];
+    [yAxisCone setFirstMaterial:yAxisMaterial];
+    [yAxisConeNode setPosition:SCNVector3Make(0, 2, 0)];
+    [yAxisRodNode addChildNode:yAxisConeNode];
+    [selectionNode addChildNode:yAxisRodNode];
+    
+    SCNCylinder *zAxisRod = [SCNCylinder cylinderWithRadius:.1 height:4];
+    SCNNode *zAxisRodNode = [SCNNode nodeWithGeometry:zAxisRod];
+    SCNMaterial *zAxisMaterial = [SCNMaterial material];
+    zAxisMaterial.diffuse.contents = [NSColor redColor];
+    [zAxisRod setFirstMaterial:zAxisMaterial];
+    
+    CATransform3D zAxisTransform = CATransform3DMakeTranslation(xAxisOffset.x, xAxisOffset.y, xAxisOffset.z);
+    zAxisTransform = CATransform3DRotate(zAxisTransform, M_PI_2, 0, 0, 1);
+    zAxisTransform = CATransform3DTranslate(zAxisTransform, 0, 2, 0);
+    [zAxisRodNode setTransform:zAxisTransform];
+    
+    SCNCone *zAxisCone = [SCNCone coneWithTopRadius:0 bottomRadius:.5 height:2];
+    SCNNode *zAxisConeNode = [SCNNode nodeWithGeometry:zAxisCone];
+    [zAxisCone setFirstMaterial:zAxisMaterial];
+    [zAxisConeNode setPosition:SCNVector3Make(0, 2, 0)];
+    [zAxisRodNode addChildNode:zAxisConeNode];
+    
+    [selectionNode addChildNode:zAxisRodNode];
+    return selectionNode;
 }
 
 @end
